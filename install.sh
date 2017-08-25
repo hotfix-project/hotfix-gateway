464b8e837877729adf443435107eeed239e6fc79
zhangzheng <zzgigi2003@163.com>
2017-08-25 08:30:12 +0000

    add module: pyhton redis

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PWD=$(cd "$(dirname "$0")"; pwd)
+
 role=`id -u`
 if test $role -ne 0
 then
@@ -10,3 +12,6 @@ fi
 curl https://raw.githubusercontent.com/WALL-E/static/master/setup/redhat/install_python36|bash
 
 pip3.6 install -r requirements.txt
+
+yum install -y redis
+/bin/cp -f ${PWD}/conf/redis.conf /etc/redis.conf
