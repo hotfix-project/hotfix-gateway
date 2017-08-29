676a6e0eaaa6297c3ca539f01b126d9422f85ea8
zhangzheng <zzgigi2003@163.com>
2017-08-29 17:25:07 +0800

    complate: cache /check_update to redis

--- app.py
+++ app.py
@@ -59,23 +59,42 @@ class ProxyHandler(tornado.web.RequestHandler):
     def get(self):
         rds = redis_context["rds"]
         if self.request.path == "/check_update":
-            print("### /check_update")
+            key = "status_%s" % (self.request.uri)
+            status = rds.get(key)
+            key = "response_%s" % (self.request.uri)
+            response = rds.get(key)
+            if response is not None and status is not None:
+                if int(status) == 200:
+                    data = json.loads(response)
+                    released = data["result"]["patch"]["released"]
+                    if len(released) == 1:
+                        key = "patch_id=%d" % (data["result"]["patch"]["released"][0]["id"])
+                        download_count = rds.hget("download_count_hash", key)
+                        pool_size = rds.hget("pool_size_hash", key)
+                        if int(download_count) > int(pool_size):
+                            self.set_status(404)
+                            self.write('{"message": "not found patch"}')
+                            self.finish()
+                            return
+                        else:
+                            rds.hincrby("download_count_hash", key)
+                self.set_status(int(status))
+                self.write(response)
+                self.finish()
+                return
+            
         if self.request.path == "/report_update":
             key = "status_%s" % (self.request.uri)
             status = rds.get(key)
             key = "response_%s" % (self.request.uri)
             response = rds.get(key)
-            if response is not None:
+            if response is not None and status is not None:
                 if int(status) == 200:
                     rds.hincrby("apply_count_hash", self.request.uri)
                 self.set_status(int(status))
                 self.write(response)
                 self.finish()
                 return
-        if self.request.path == "/custom_check_update":
-            print("### /custom_check_update")
-        if self.request.path == "/custom_report_update":
-            print("### /custom_report_update")
         self._do_fetch('GET')
 
     def _do_fetch(self, method):
@@ -102,7 +121,21 @@ class ProxyHandler(tornado.web.RequestHandler):
                 logger.error("Tornado signalled HTTPError %s", x)
 
     def cache_check_update(self, status_code, response):
-        pass
+        print("cache_check_update")
+        rds = redis_context["rds"]
+        key = "status_%s" % (self.request.uri)
+        rds.set(key, status_code, options.redis_ttl)
+        key = "response_%s" % (self.request.uri)
+        rds.set(key, response.body, options.redis_ttl)
+        if status_code == 200:
+            data = json.loads(response.body)
+            released = data["result"]["patch"]["released"]
+            if len(released) == 1:
+                key = "patch_id=%s"%(released[0]["id"])
+                pool_size = released[0]["pool_size"]
+                rds.hset("pool_size_hash", key, pool_size)
+                rds.hsetnx("download_count_hash", key, 0)
+                rds.hincrby("download_count_hash", self.request.uri)
 
     def cache_report_update(self, status_code, response):
         rds = redis_context["rds"]
@@ -113,12 +146,6 @@ class ProxyHandler(tornado.web.RequestHandler):
         rds.hsetnx("apply_count_hash", self.request.uri, 0)
         if status_code == 200:
             rds.hincrby("apply_count_hash", self.request.uri)
-
-    def cache_custom_check_update(self, status_code, response):
-        pass
-
-    def cache_custom_report_update(self, status_code, response):
-        pass
 
     def _on_proxy(self, response):
         self.clear()
@@ -136,10 +163,6 @@ class ProxyHandler(tornado.web.RequestHandler):
             self.cache_check_update(status_code, response)
         if self.request.path == "/report_update":
             self.cache_report_update(status_code, response)
-        if self.request.path == "/custom_check_update":
-            self.cache_custom_check_update(status_code, response)
-        if self.request.path == "/custom_report_update":
-            self.cache_custom_report_update(status_code, response)
 
         self.finish()
 
